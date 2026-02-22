e38228475a8ba7d3831c4013f7f17583758c9048
TheFirstSpine <teddy@thefirstspine.fr>
2026-02-22 07:16:46 +0100

    hotfix(utils): fix top-bottom effects

--- src/utils/game.utils.ts
+++ src/utils/game.utils.ts
@@ -31,15 +31,15 @@ export function getSubjectiveSides(userId: number, gameInstance: IGameInstance) 
     return [
       {x: 1, y: 0},
       {x: -1, y: 0},
-      {x: 0, y: 1},
       {x: 0, y: -1},
+      {x: 0, y: 1},
     ];
   }
 
   return [
       {x: -1, y: 0},
       {x: 1, y: 0},
-      {x: 0, y: -1},
       {x: 0, y: 1},
+      {x: 0, y: -1},
     ];
 }
